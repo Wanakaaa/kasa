f83eff1bbf9937683b6bf583304f97432309f343
Sarah Colin <srh.colin@gmail.com>
2024-10-10 11:42:33 +0200

    test header

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import { Routes, Route } from 'react-router-dom'
 import Home from "./templates/Home";
 import About from "./templates/About";
 import Contact from "./templates/Contact";
-import Navbar from "./Navbar";
+import Header from "./components/Header/Header"
+// import Navbar from "./Navbar";
 
 function App() {
   return (
-    <>
-      <Navbar />
+    <div className='app'>
+      <header>
+        <Header />
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+// import Navbar from '../Header/Navbar/Navbar'
+import logoHeader from '../../assets/LogoHeader.png'
+import { Link } from 'react-router-dom';
+
+
+function Header() {
+  return (
+    <div className='header-container'> 
+      <Link to={`/`}>
+        <img 
+            src={logoHeader} 
+            alt='logo KASA' 
+            className='logo'></img>
+      </Link>
+        
+      {/* <Navbar
+      className='nav'
+      /> */}
+    </div>
+  )
+}
+
+export default Header   
